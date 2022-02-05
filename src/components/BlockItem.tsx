dd9d80bc442fa1ff96094d32f8f39ba07a4435e2
kobakazu0429 <kobakazu10@gmail.com>
2022-02-05 21:17:36 +0900

    update BlockItem

--- src/components/BlockItem.tsx
+++ src/components/BlockItem.tsx
@@ -1,13 +1,11 @@
 import type { VFC } from "react";
 import {
   AspectRatio,
-  Flex,
   Box,
   Image,
-  chakra,
-  Tooltip,
+  Text,
+  Heading,
   WrapItem,
-  useColorModeValue,
 } from "@chakra-ui/react";
 
 interface Props {
@@ -19,53 +17,37 @@ interface Props {
 export const BlockItem: VFC<Props> = ({ coverImageUrl, name, price }) => {
   return (
     <WrapItem w="220px">
-      <Box
-        bg={useColorModeValue("white", "gray.800")}
-        w={220}
-        borderWidth="1px"
-        rounded="lg"
-        shadow="lg"
-        position="relative"
-      >
-        <AspectRatio ratio={1}>
-          <Image
-            src={coverImageUrl}
-            alt={`Picture of ${name}`}
-            roundedTop="lg"
-            fit="cover"
-          />
-        </AspectRatio>
+      <Box bg="white" w={220} borderWidth="1px" rounded="lg" shadow="lg">
+        <Box>
+          <AspectRatio ratio={1}>
+            <Image
+              src={coverImageUrl}
+              alt={`Picture of ${name}`}
+              roundedTop="lg"
+              fit="cover"
+            />
+          </AspectRatio>
+        </Box>
 
-        <Box p="6">
-          <Flex mt="1" justifyContent="space-between" alignContent="center">
-            <Box
-              fontSize="2xl"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {name ?? "no name"}
-            </Box>
-            <Tooltip
-              label="Add to cart"
-              bg="white"
-              placement={"top"}
-              color={"gray.800"}
-              fontSize={"1.2em"}
-            >
-              <chakra.a href={"#"} display={"flex"}></chakra.a>
-            </Tooltip>
-          </Flex>
+        <Box pt="3" px="3">
+          <Heading
+            fontSize="2xl"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            {name ?? "no name"}
+          </Heading>
+        </Box>
 
-          <Flex justifyContent="space-between" alignContent="center">
-            <Box fontSize="2xl" color={useColorModeValue("gray.800", "white")}>
-              <Box as="span" color={"gray.600"} fontSize="lg">
-                &yen;
-              </Box>
-              {price}
-            </Box>
-          </Flex>
+        <Box pb="3" px="3">
+          <Text fontSize="xl" color="gray.800">
+            <Text as="span" color={"gray.600"} fontSize="md">
+              &yen;{" "}
+            </Text>
+            {price}
+          </Text>
         </Box>
       </Box>
     </WrapItem>
