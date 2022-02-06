248f2f442f48e77fb5f69f1786879e3d5028ca0a
kobakazu0429 <kobakazu10@gmail.com>
2022-02-06 15:16:20 +0900

    analyze next build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true"
+    ? require("@next/bundle-analyzer")({ enabled: true })
+    : (config) => config;
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["tfansite.jp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
