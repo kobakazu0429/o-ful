a67273e877359516667ab2cc2bfa040f9dd435ea
kobakazu0429 <kobakazu10@gmail.com>
2022-02-07 00:53:10 +0900

    add session type

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,14 @@
+export {};
+
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      name: string;
+      email: string;
+      image: string;
+      uid: string;
+      twitterId: string;
+    };
+    expires: ISODateString;
+  }
+}
