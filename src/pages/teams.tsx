bf43f84d6e92d45b5cef4c92d3ed8a4ce89decb7
kobakazu0429 <kobakazu10@gmail.com>
2022-02-07 17:59:15 +0900

    add page

---
+++ src/pages/teams.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { TextPageLayout, Content } from "../layouts/TextPage";
+
+const Teams: NextPage = () => {
+  return (
+    <TextPageLayout title="利用規約">
+      <Content title="">利用規約ページです</Content>
+    </TextPageLayout>
+  );
+};
+
+export default Teams;
