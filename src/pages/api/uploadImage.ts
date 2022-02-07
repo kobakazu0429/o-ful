704f97fa14ec2de42116e4a88af11df2b904a506
kobakazu0429 <kobakazu10@gmail.com>
2022-02-08 00:07:37 +0900

    update

--- src/pages/api/uploadImage.ts
+++ src/pages/api/uploadImage.ts
@@ -3,12 +3,6 @@ import cloudinary from "cloudinary";
 import { IncomingForm } from "formidable";
 import type { Fields, Files } from "formidable";
 
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 export const config = {
   api: {
     bodyParser: false,
@@ -19,6 +13,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME?.trim(),
+    api_key: process.env.CLOUDINARY_API_KEY?.trim(),
+    api_secret: process.env.CLOUDINARY_API_SECRET?.trim(),
+  });
+
   try {
     const data = await new Promise<{
       fields: Fields;
