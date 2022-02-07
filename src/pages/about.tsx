90af506de48fba1af4861dbcf0d22429158a32e5
kobakazu0429 <kobakazu10@gmail.com>
2022-02-07 17:44:34 +0900

    add some page

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { TextPageLayout } from "../layouts/TextPage";
+
+const Guide: NextPage = () => {
+  return <TextPageLayout title="o-ful">o-fulってなに</TextPageLayout>;
+};
+
+export default Guide;
---
+++ src/pages/guide.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { TextPageLayout } from "../layouts/TextPage";
+
+const Guide: NextPage = () => {
+  return <TextPageLayout title="使い方">使い方ページ</TextPageLayout>;
+};
+
+export default Guide;
