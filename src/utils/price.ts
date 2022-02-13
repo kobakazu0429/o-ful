8c2a8ea7958d8c126fd2fe2fe0c7b424f2f76e4c
kobakazu0429 <kobakazu10@gmail.com>
2022-02-14 01:14:04 +0900

    fix price is 0

--- src/utils/price.ts
+++ src/utils/price.ts
@@ -1,5 +1,5 @@
 export function formatPrice(value?: number) {
-  if (!value) return "不明";
+  if (value === undefined) return "不明";
   const formatter = new Intl.NumberFormat("ja-JP", {
     style: "currency",
     currency: "JPY",
